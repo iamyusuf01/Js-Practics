4aa948f720dc5bd70ec01d8d568aef66583d8ec5
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2024-09-28 16:26:01 +0530

    update

---
+++ JavaScript Practice/triangle.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 12;
+let c = 20;
+
+if (a + b > c && a + c > b && b + c > a) {
+    console.log("The given sides can form a triangle");
+}
+else {
+    console.log("The given sides can not form a triangle");
+}
