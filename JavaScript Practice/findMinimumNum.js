9ebbda3e01a48a3d3d608e60b243208e16321f68
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2024-09-28 16:03:58 +0530

    update

---
+++ JavaScript Practice/findMinimumNum.js
@@ -0,0 +1,14 @@
+let x = 40;
+let y = 50;
+let z= 30;
+
+if(x < y && x < z){
+    console.log("x is the smallest");
+}
+else if(y < x && y < z )
+    {
+    console.log("y is the smallest");
+    }
+else {
+    console.log("z is the smallest");
+}
